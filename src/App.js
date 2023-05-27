e907707d7d3143a2929d0bc43d3ee3e2b7f8af9e
RiyaBanik <97799654+RiyaBanik@users.noreply.github.com>
2023-05-27 14:09:14 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Restaurent from './component/Basics/restaurent'
 import UseState from './component/Hooks/useState';
 const App = () => {
-  return <UseState />
+  // return <UseState />
+  return <Restaurent />
 }
 
 export default App
